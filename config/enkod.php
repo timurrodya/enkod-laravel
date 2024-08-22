dc22edb7ebe8068f638d1e263d253936d0202f27
timurrodya <timurrodya@mail.ru>
2024-08-22 22:17:29 +1000

    fix(enkod): метод для создания шаблона сообщения

--- config/enkod.php
+++ config/enkod.php
@@ -1,1 +1,10 @@
-<?phpreturn [    /*     * API token to Enkod     */    'apiKey'  => env('ENKOD_API_KEY', ''),    'baseurl' => env('ENKOD_BASE_URL', 'https://api.enkod.ru/'),    'version' => env('ENKOD_VERSION', 'v1'),];
+<?php
+
+return [
+    /*
+     * API token to Enkod
+     */
+    'apiKey'  => env('ENKOD_API_KEY', ''),
+    'baseurl' => env('ENKOD_BASE_URL', 'https://api.enkod.ru/'),
+    'version' => env('ENKOD_VERSION', 'v1'),
+];
