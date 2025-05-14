132e36b7738d7f5d642b79b8a216fc6f3a884ca0
Тимур Родя <timurrodya@mail.ru>
2025-05-14 15:19:58 +1000

    refactor(ApiException): объединение форматирования ошибок

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -10,23 +10,54 @@ class ApiException extends Exception
     /**
      * @param  int  $code
      * @param  string|null  $message
-     * @param  Throwable|null  $previous
+     * @param  Throwable|null  $previous  ,
+     * @param  string|null  $requestId  ID запроса из заголовков
      *
      * @throws Exception
      */
     public function __construct(
         int $code = 0,
-        string $message = null,
+        ?string $message = null,
         Throwable $previous = null,
+        ?string $requestId = null
     ) {
-        $developer_message = match ($code) {
+        $baseMessage = match ($code) {
             400 => 'Ошибка в запросе. Подробная информация в ответе:',
             401 => 'У API ключа нет прав на выполнение этого действия',
             404 => 'Not found',
             500 => 'Что-то пошло не так. Свяжитесь со своим персональным менеджером',
             default => throw new Exception('Неизвестный код ответа'),
         };
-        $message = sprintf('%s %s', $developer_message, $message);
-        parent::__construct($message, $code, $previous);
+        parent::__construct(
+            $this->formatErrorMessage($baseMessage, $message, $requestId),
+            $code,
+            $previous
+        );
+    }
+
+    protected function formatErrorMessage(
+        string $baseMessage,
+        ?string $apiMessage,
+        ?string $requestId,
+    ): string {
+        $parts = [$baseMessage];
+
+        if ($requestId) {
+            $parts[] = "Request ID: {$requestId}";
+        }
+
+        if ($apiMessage) {
+            $parts[] = "Сообщение: {$apiMessage}";
+        }
+
+        return implode('. ', $parts);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'code'    => $this->code,
+            'message' => $this->message,
+        ];
     }
 }
